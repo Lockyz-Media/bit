339a4122ec6b81f49ce4dd8cee2a50e7d85c5cd1
Robin P <lochlanpainter@gmail.com>
2023-08-30 14:14:59 +1000

    Backwards Compatability

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Bit Core
-Current Version: 4.1.0
-Next Version: 5.0.0
+## Version Information
+| Current Version | Next Version |
+| :---: | :---: |
+| 4.1.0 | 5.0.0 |
+
+## Whats included in the core version?
+- Command Handler
+- Event Handler
+- Some small commands
+
+## Information about the 4.1.0 update
+The 4.1.0 update is a small patch to include the new configuration format *but with the deprecated values to help with backwards compatability* and the custom status system added to discord.js v14.13.0
--- events/ready.js
+++ events/ready.js
@@ -1,45 +1,54 @@
 const { Events } = require('discord.js');
-const { embedColours, botIDs, activity } = require('../config');
+const { embedColours, botIDs, activities, activity, status } = require('../config');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		var statusActivityType = ActivityType.Playing
-		var statusActivityName = "string"
-		var statusActivityState = "string"
-		var activityStatus = "online"
+		if(activities.version === "new") {
+			var statusActivityType = ActivityType.Playing
+			var statusActivityName = "string"
+			var statusActivityState = "string"
+			var activityStatus = "online"
 
-		if(activity.type === "custom") {
-			statusActivityType = ActivityType.Custom
-		} else if(activity.type === "playing") {
-			statusActivityType = ActivityType.Playing
-		} else if(activity.type === "listening") {
-			statusActivityType = ActivityType.Listening
-		} else if(activity.type === "watching") {
-			statusActivityType = ActivityType.Watching
+			if(activities.type === "custom") {
+				statusActivityType = ActivityType.Custom
+			} else if(activities.type === "playing") {
+				statusActivityType = ActivityType.Playing
+			} else if(activities.type === "listening") {
+				statusActivityType = ActivityType.Listening
+			} else if(activities.type === "watching") {
+				statusActivityType = ActivityType.Watching
+			}
+
+			if(activities.state) {
+				statusActivityState = activities.state
+			}
+
+			if(activities.status) {
+				activityStatus = activities.status
+			}
+
+			client.user.setPresence({
+				activities: [{
+					type: statusActivityType,
+					name: "status",
+					state: statusActivityState
+				}],
+				status: activityStatus
+			})
+		} else {
+			client.user.setPresence({
+				activities: [
+					{
+						type: ActivityType.Playing,
+						name: "status",
+						state: activity
+					}
+				],
+				status: status
+			})
 		}
-
-		if(activity.name) {
-			statusActivityName = activity.name
-		}
-
-		if(activity.state) {
-			statusActivityState = activity.state
-		}
-
-		if(activity.status) {
-			activityStatus = activity.status
-		}
-
-		client.user.setPresence({
-			activities: [{
-				type: statusActivityType,
-				name: statusActivityName,
-				state: statusActivityState
-			}],
-			status: activityStatus
-		})
 
 		console.log('Status Set')
 		console.log('🟢 Bit Core: 4.1.0 Online! Logged in as '+ client.user.tag)
