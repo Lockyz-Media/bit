9e281d124853d7001fcfd524b63e85eb5429e53d
Robin P <lochlanpainter@gmail.com>
2023-10-09 12:41:06 +1100

    Readded the commands to the main bot files, I couldn't get working what I wanted to do :(

--- bit.js
+++ bit.js
@@ -40,6 +40,21 @@ process.on('unhandledRejection', error => {
 client.commands = new Collection();
 client.plugins = new Collection();
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	if('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+console.log("Loading "+commandFiles.length+" commands")
+
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
---
+++ commands/plugins.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder, version: discordVersion, SlashCommandBuilder } = require('discord.js')
+const wait = require('node:timers/promises').setTimeout;
+const fs = require('node:fs');
+const path = require('node:path');
+const moment = require('moment');
+require('moment-duration-format');
+const bit = require('./../../../bit.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('plugins')
+		.setDescription('List all plugins available within the bot')
+        .setDMPermission(true),
+	async execute(interaction) {
+        const client = interaction.client
+        interaction.deferReply()
+        await wait(4000);
+        var pluginCount = 0;
+        var embedDescription = '';
+
+        const embed = new EmbedBuilder()
+            .setTitle('Plugin List')
+
+        for(const plugin in bit.listAllPlugins()) {
+            embedDescription += plugin.name+" by "+plugin.developer+"\n"
+        }
+
+        if(pluginCount === bit.countPlugins) {
+            interaction.reply({ embeds: [embed] })
+        }
+        //interaction.reply({ content: "Test works" })
+        //interaction.reply({ content: client.plugins.stringify() })
+        /*client.plugins.forEach(element => {
+            interaction.channel.send({ content: element })
+        });*/
+        //var pluginList = new Array
+	}
+};
