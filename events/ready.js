4d891ca8d3a15681bdafa0a5d5736065b9cc02c4
Robin P <lochlanpainter@gmail.com>
2024-05-06 13:11:35 +1000

    Updated bit-core, updated the package.json and ready.js files to use more accurate values

--- events/ready.js
+++ events/ready.js
@@ -38,7 +38,7 @@ module.exports = {
 		})
 
 		console.log('Status Set')
-		console.log('🟢 Bit Core: 5.0.0 Online! Logged in as '+ client.user.tag)
+		console.log('🟢 Bit Core: 5.2.1 Online! Logged in as '+ client.user.tag)
 		console.log('==== Have a good day! ====');
 	}
 }
