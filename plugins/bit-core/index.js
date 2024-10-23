c71d1e7af46dca242451011a467fa13925bec061
Robin Painter <lochlanpainter@gmail.com>
2024-10-23 16:54:16 +1100

    Individual log types can now be disabled in the global config.

--- plugins/bit-core/index.js
+++ plugins/bit-core/index.js
@@ -1,4 +1,5 @@
 const emojiCharacters = require("./emojiCharacters.js")
+const config = require("../../config.json")
 module.exports = {
     start_function: function start_function() {
         console.log("Bit: Core Successfully Loaded!")
@@ -6,19 +7,27 @@ module.exports = {
     find_emoji: function find_emoji(emoji) {
         return emojiCharacters[emoji];
     },
-    log: function log(type, plugin, message) {
+    log: function log(type, plugin, force, message) {
         if(isNaN(type)) {
             if(plugin) {
                 if(message) {
-                    console.log("[INFO] "+plugin+": "+message)
+                    if(config.logs.send_to_console.info || force) {
+                        console.log("[INFO] "+plugin+": "+message)
+                    }
                 } else {
-                    console.log("[ERROR] "+plugin+", has tried to send a log without a message.")
+                    if(config.logs.send_to_console.errors || force) {
+                        console.log("[ERROR] "+plugin+", has tried to send a log without a message.")
+                    }
                 }
             } else {
                 if(message) {
-                    console.log("[INFO] Unknown Plugin: "+message)
+                    if(config.logs.send_to_console.info || force) {
+                        console.log("[INFO] Unknown Plugin: "+message)
+                    }
                 } else {
-                    console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                    if(config.logs.send_to_console.errors || force) {
+                        console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                    }
                 }
             }
         } else {
@@ -26,60 +35,92 @@ module.exports = {
                 case 0:
                     if(plugin) {
                         if(message) {
-                            console.log("[INFO] "+plugin+": "+message)
+                            if(config.logs.send_to_console.info || force) {
+                                console.log("[INFO] "+plugin+": "+message)
+                            }
                         } else {
-                            console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            }
                         }
                     } else {
                         if(message) {
-                            console.log("[INFO] Unknown Plugin: "+message)
+                            if(config.logs.send_to_console.info || force) {
+                                console.log("[INFO] Unknown Plugin: "+message)
+                            }
                         } else {
-                            console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            }
                         }
                     }
                 break;
                 case 1: 
                     if(plugin) {
                         if(message) {
-                            console.log("[WARNING] "+plugin+": "+message)
+                            if(config.logs.send_to_console.warnings || force) {
+                                console.log("[WARNING] "+plugin+": "+message)
+                            }
                         } else {
-                            console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            }
                         }
                     } else {
                         if(message) {
-                            console.log("[WARNING] Unknown Plugin: "+message)
+                            if(config.logs.send_to_console.warnings || force) {
+                                console.log("[WARNING] Unknown Plugin: "+message)
+                            }
                         } else {
-                            console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            }
                         }
                     }
                 break;
                 case 2:
                     if(plugin) {
                         if(message) {
-                            console.log("[ERROR] "+plugin+": "+message)
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] "+plugin+": "+message)
+                            }
                         } else {
-                            console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            }
                         }
                     } else {
                         if(message) {
-                            console.log("[ERROR] Unknown Plugin: "+message)
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] Unknown Plugin: "+message)
+                            }
                         } else {
-                            console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            }
                         }
                     }
                 break;
                 default: 
                     if(plugin) {
                         if(message) {
-                            console.log("[INFO] "+plugin+": "+message)
+                            if(config.logs.send_to_console.info || force) {
+                                console.log("[INFO] "+plugin+": "+message)
+                            }
                         } else {
-                            console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] "+plugin+", has tried to send a log without a message ")
+                            }
                         }
                     } else {
                         if(message) {
-                            console.log("[INFO] Unknown Plugin: "+message)
+                            if(config.logs.send_to_console.info || force) {
+                                console.log("[INFO] Unknown Plugin: "+message)
+                            }
                         } else {
-                            console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            if(config.logs.send_to_console.errors || force) {
+                                console.log("[ERROR] Unknown Plugin, has tried to send a log without a message.")
+                            }
                         }
                     }
                 break;
