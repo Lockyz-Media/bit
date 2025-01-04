d6c5fca448778efeffe53e10c0ec390155b2ac3d
Robin Painter <lochlanpainter@gmail.com>
2025-01-04 19:36:52 +1100

    Now the actual plugin requirement check happens just after the compatibility check and just before the plugins commands and events are added, this means that the requirement check is done multiple times for each plugin, but also means the requirement check is more efficient.

--- bit.js
+++ bit.js
@@ -77,6 +77,7 @@ if(plugin_path && plugins) {
 		const plugins_json = fs.readFileSync('./databases/bit/plugins.json');
 		const jsonData = JSON.parse(plugins_json);
 		var plugin_info = new Array()
+		var compatible = true;
 
 		if(plugin_file.id) {
 			plugin_info.id = plugin_file.id
@@ -120,12 +121,8 @@ if(plugin_path && plugins) {
 			plugin_info.disabled = false
 			disabled = false;
 		}
-
-		var compatible = true;
-
-		if(disabled === false) {
-			const plugin_data = require(plugin_path+"/"+folder+"/plugin.json")
-			if(plugin_data.id === "bit-core") {
+		if(plugin_info.disabled === false) {
+			if(plugin_file.id === "bit-core") {
 				if(no_core === false) {
 					core.log(2, "Bit", true, 'A plugin is calling itself bit core, but bit core is already installed. Please review your plugins. Exiting...')
 					process.exit(1);
@@ -134,18 +131,18 @@ if(plugin_path && plugins) {
 				}
 			}
 
-			if(plugin_data.id === "example-plugin") {
+			if(plugin_file.id === "example-plugin") {
 				if(dev_mode) {
-					core.log(0, "Dev-Mode", true, `The plugin with the name ${plugin_data.name} has the ID 'example-plugin'. Make sure to change your plugins ID before releasing your plugin.`)
+					core.log(0, "Dev-Mode", true, `The plugin with the name ${plugin_file.name} has the ID 'example-plugin'. Make sure to change your plugins ID before releasing your plugin.`)
 				} else {
-					core.log(2, "Bit", true, `The plugin with the name ${plugin_data.name} has the ID 'example-plugin'. This means the developer has likely not updated their plugin.json and things may not work as intended!`)
+					core.log(2, "Bit", true, `The plugin with the name ${plugin_file.name} has the ID 'example-plugin'. This means the developer has likely not updated their plugin.json and things may not work as intended!`)
 				}
 			}
 
-			core.log(0, "Bit", true, "Loading "+plugin_data.name+" made by "+plugin_data.developer)
-			client.plugins.set(plugin_data.name)
-			if(plugin_data.update_url) {
-				let url = plugin_data.update_url;
+			core.log(0, "Bit", true, "Loading "+plugin_file.name+" made by "+plugin_file.developer)
+			client.plugins.set(plugin_file.name)
+			if(plugin_file.update_url) {
+				let url = plugin_file.update_url;
 				let update = ""
 
 				let settings = { method: "Get" };
@@ -153,29 +150,142 @@ if(plugin_path && plugins) {
 					.then(res => res.json())
 					.then((json) => {
 						update = json.bit_versions[bit_version.major]
-						if(update === plugin_data.version) {
+						if(update === plugin_file.version) {
 						} else {
-							core.log(1, "Bit", true, "Plugin "+plugin_data.name+" by "+plugin_data.developer+" is outdated, it may be dangerous to continue without updating.")
-							core.log(1, "Bit", true, "Installed Version: "+plugin_data.version)
+							core.log(1, "Bit", true, "Plugin "+plugin_file.name+" by "+plugin_file.developer+" is outdated, it may be dangerous to continue without updating.")
+							core.log(1, "Bit", true, "Installed Version: "+plugin_file.version)
 							core.log(1, "Bit", true, "Latest Version: "+update)
 							core.log(1, "Bit", true, "Update from "+json.download_Link)
 						}
 					})
 			} else {
-				core.log(1, "Bit", true, "Plugin "+plugin_data.name+" does not include an update_url and may be outdated.")
+				core.log(1, "Bit", true, "Plugin "+plugin_file.name+" does not include an update_url and may be outdated.")
 			}
 
-			if(plugin_data.requirements.bit) {
-				if(plugin_data.requirements.bit.version === bit_version.full) {
+			if(plugin_file.requirements.bit) {
+				if(plugin_file.requirements.bit.version === bit_version.full) {
 					compatible = true;
 				} else {
-					core.log(2, "Bit", true, "Plugin "+plugin_data.name+" was not made for this version of Bit, there WILL be compatability issues.")
+					core.log(2, "Bit", true, "Plugin "+plugin_file.name+" was not made for this version of Bit, there WILL be compatability issues.")
 					compatible = false;
 				}
 			} else {
-				core.log(2, "Bit", true, "Plugin "+plugin_data.name+" does not specify a Bit version, it may have been built for an older version of the bot.")
+				core.log(2, "Bit", true, "Plugin "+plugin_file.name+" does not specify a Bit version, it may have been built for an older version of the bot.")
 				compatible = false;
 			}
+
+			if(compatible === false) {
+				plugin_info.disabled = true;
+				core.log(1, "Bit", true, plugin_info.name+" has an error and has been disabled!")
+			}
+		}
+
+		jsonData.plugins.push({
+			id: plugin_info.id,
+			disabled: plugin_info.disabled,
+			name: plugin_info.name,
+			version: plugin_info.version,
+			has_index: plugin_info.has_index,
+			main_file: plugin_info.main_file,
+			requirements: plugin_info.requirements
+		})
+
+		const jsonString = JSON.stringify(jsonData);
+		fs.writeFileSync('./databases/bit/plugins.json', jsonString, 'utf-8', (err) => {
+			if (err) throw err;
+		  });
+
+		  fs.readFile('./databases/bit/plugins.json', 'utf8', (err, data) => {
+			if(err) {
+				core.log(0, "Bit", true, "Cannot read plugins database: "+err)
+				process.exit(1);
+			}
+		
+			const plugins_database = JSON.parse(data);
+		
+			const plugin_ids = plugins_database.plugins.map((plugin) => ({
+				[plugin.id]: { version: plugin.version, disabled: plugin.disabled }
+			}))
+		
+			plugins_database.plugins.forEach((plugin_info) => {
+				let allRequirementsExist = true;
+			
+				//console.log(plugin_info.requirements)
+				Object.entries(plugin_info.requirements).forEach(([reqName, reqDetails]) => {
+					if(reqName === "bit") {
+						reqName = "bit-core"
+					}
+			
+					const requiredVersion = reqDetails.version;
+					var reqExists
+					var softReq
+			
+					if(reqDetails.level === "0") {
+						const requirementExists = plugin_ids.some((pluginMap) => 
+							pluginMap.hasOwnProperty(reqName) &&
+							pluginMap[reqName].version === requiredVersion &&
+							pluginMap[reqName].disabled === false
+						);
+			
+						if(!requirementExists) {
+							core.log(2, "Bit", true, `Requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found, plugin will be disabled as this is a hard requirement!`)
+							reqExists = false;
+							softReq = false;
+						}
+					} else if(reqDetails.level === "1") {
+						const requirementExists = plugin_ids.some((pluginMap) => 
+							pluginMap.hasOwnProperty(reqName) &&
+							pluginMap[reqName].disabled === false
+						);
+				
+						if(!requirementExists) {
+							core.log(2, "Bit", true, `Requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found, plugin will be disabled as this is a hard requirement!`)
+							reqExists = false;
+							softReq = false;
+						}
+					} else if(reqDetails.level === "2") {
+						const requirementExists = plugin_ids.some((pluginMap) =>
+							pluginMap.hasOwnProperty(reqName) &&
+							pluginMap[reqName].disabled === false
+						);
+			
+						if(!requirementExists) {
+							core.log(1, "Bit", true, `Soft requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found. Some features may be disabled!`)
+							reqExists = false;
+							softReq = true;
+						}
+					} else if(reqDetails.level === "3") {
+						const requirementExists = plugin_ids.some((pluginMap) =>
+							pluginMap.hasOwnProperty(!reqName)
+						);
+			
+						if(!requirementExists) {
+							core.log(1, "Bit", true, `Plugin "${reqName}" is incompatible with "${plugin_info.name}". "${plugin_info.name}" will be disabled.`)
+							reqExists = false;
+							softReq = false;
+						}
+					}
+			
+					if(reqExists === false && softReq === false) {
+						allRequirementsExist = false;
+					}
+				})
+			
+				if(allRequirementsExist === false) {
+					plugin_info.disabled = true;
+				}
+			})
+		
+			fs.writeFile('./databases/bit/plugins.json', JSON.stringify(plugins_database, null, 2), 'utf8', (err) => {
+				if(err) {
+					core.log(0, "Bit", true, "Cannot write to plugins json: "+ err);
+					process.exit(1);
+				}
+			})
+		})
+		
+		if(disabled === false) {
+			const plugin_data = require(plugin_path+"/"+folder+"/plugin.json")
 
 			if(compatible === true) {
 				if(plugin_data.commands === true) {
@@ -223,119 +333,10 @@ if(plugin_path && plugins) {
 		} else {
 			core.log(1, "Bit", true, plugin_info.name+" has an error and has been disabled!")
 		}
-
-		if(compatible === false) {
-			plugin_info.disabled = true;
-			core.log(1, "Bit", true, plugin_info.name+" has an error and has been disabled!")
-		}
-
-		jsonData.plugins.push({
-			id: plugin_info.id,
-			disabled: plugin_info.disabled,
-			name: plugin_info.name,
-			version: plugin_info.version,
-			has_index: plugin_info.has_index,
-			main_file: plugin_info.main_file,
-			requirements: plugin_info.requirements
-		})
-
-		const jsonString = JSON.stringify(jsonData);
-		fs.writeFileSync('./databases/bit/plugins.json', jsonString, 'utf-8', (err) => {
-			if (err) throw err;
-		  });
 	}
 } else {
 	core.log(1, "Bit", true, "No plugins found")
 }
-
-fs.readFile('./databases/bit/plugins.json', 'utf8', (err, data) => {
-	if(err) {
-		core.log(0, "Bit", true, "Cannot read plugins database: "+err)
-		process.exit(1);
-	}
-
-	const plugins_database = JSON.parse(data);
-
-	const plugin_ids = plugins_database.plugins.map((plugin) => ({
-		[plugin.id]: { version: plugin.version, disabled: plugin.disabled }
-	}))
-
-	plugins_database.plugins.forEach((plugin_info) => {
-		let allRequirementsExist = true;
-	
-		//console.log(plugin_info.requirements)
-		Object.entries(plugin_info.requirements).forEach(([reqName, reqDetails]) => {
-			if(reqName === "bit") {
-				reqName = "bit-core"
-			}
-	
-			const requiredVersion = reqDetails.version;
-			var reqExists
-			var softReq
-	
-			if(reqDetails.level === "0") {
-				const requirementExists = plugin_ids.some((pluginMap) => 
-					pluginMap.hasOwnProperty(reqName) &&
-					pluginMap[reqName].version === requiredVersion &&
-					pluginMap[reqName].disabled === false
-				);
-	
-				if(!requirementExists) {
-					core.log(2, "Bit", true, `Requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found, plugin will be disabled as this is a hard requirement!`)
-					reqExists = false;
-					softReq = false;
-				}
-			} else if(reqDetails.level === "1") {
-				const requirementExists = plugin_ids.some((pluginMap) => 
-					pluginMap.hasOwnProperty(reqName) &&
-					pluginMap[reqName].disabled === false
-				);
-		
-				if(!requirementExists) {
-					core.log(2, "Bit", true, `Requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found, plugin will be disabled as this is a hard requirement!`)
-					reqExists = false;
-					softReq = false;
-				}
-			} else if(reqDetails.level === "2") {
-				const requirementExists = plugin_ids.some((pluginMap) =>
-					pluginMap.hasOwnProperty(reqName) &&
-					pluginMap[reqName].disabled === false
-				);
-	
-				if(!requirementExists) {
-					core.log(1, "Bit", true, `Soft requirement "${reqName}" for plugin "${plugin_info.name}" at version "${reqDetails.version}" was not found. Some features may be disabled!`)
-					reqExists = false;
-					softReq = true;
-				}
-			} else if(reqDetails.level === "3") {
-				const requirementExists = plugin_ids.some((pluginMap) =>
-					pluginMap.hasOwnProperty(!reqName)
-				);
-	
-				if(!requirementExists) {
-					core.log(1, "Bit", true, `Plugin "${reqName}" is incompatible with "${plugin_info.name}". "${plugin_info.name}" will be disabled.`)
-					reqExists = false;
-					softReq = false;
-				}
-			}
-	
-			if(reqExists === false && softReq === false) {
-				allRequirementsExist = false;
-			}
-		})
-	
-		if(allRequirementsExist === false) {
-			plugin_info.disabled = true;
-		}
-	})
-
-	fs.writeFile('./databases/bit/plugins.json', JSON.stringify(plugins_database, null, 2), 'utf8', (err) => {
-		if(err) {
-			core.log(0, "Bit", true, "Cannot write to plugins json: "+ err);
-			process.exit(1);
-		}
-	})
-})
 
 if(no_core === true) {
 	core.log(2, "Bit", true, "Bot failed to start: Bit Core was not found, please redownload the bot.")
